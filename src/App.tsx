43973a2914942b562cb7593b2029848b12ac9c29
vika pika <dbvicdb@gmail.com>
2024-08-04 09:55:22 +0200

    add button logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
-import icon from './icon/user-icon.svg'
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import BgColor from './components/BgColor/BgColor';
 import LoginForm from './components/LoginForm/LoginForm';
 
 function App() {
-  const [auth, setAuth] = useState<any>(null)
+  const getAuth = () => {
+    const str = sessionStorage.getItem('auth')
+    return str != null ? JSON.parse(str) : null;
+  }
+  
+  const [auth, setAuth] = useState<any>(getAuth)
   const [loginShow, setLoginShow] = useState<any>(true)
+
+  const setAndSaveAuth = (auth: any) => {
+    sessionStorage.setItem('auth', JSON.stringify(auth))
+    setAuth(auth)
+  }
 
   return (
     <div className="App">
-      <Header auth={auth} setLoginShow={setLoginShow}/>
+      <Header auth={auth} setLoginShow={setLoginShow} setAuth={setAndSaveAuth}/>
 
      <section>
       <BgColor/>
-      {!auth && loginShow ? <LoginForm setAuth={setAuth} setLoginShow={setLoginShow}/> : null}
+      {!auth && loginShow ? <LoginForm setAuth={setAndSaveAuth} setLoginShow={setLoginShow}/> : null}
      </section>
 
       <Footer/>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import userIicon from '../../icon/user-icon.svg'
 import burgerIicon from '../../icon/burger-icon.svg'
 
 
-function Header(props: {auth: any; setLoginShow: any}) {
+function Header(props: {auth: any; setLoginShow: any; setAuth: any}) {
   const [showMobile, setShowMobile] = useState(false);
 
   return (
@@ -19,14 +19,24 @@ function Header(props: {auth: any; setLoginShow: any}) {
             <a href='#' className='main-elem roboto-regular'>About</a>
           </div>
         </div>
-        {!props.auth ? (<button className='btn-login roboto-regular header-btn-login' type='button' onClick={e => props.setLoginShow(true)}>Login</button>) : (<div className='user-name'>{props.auth.username}</div>)}        
+        {!props.auth 
+        ? (<button className='btn-login roboto-regular header-btn-login' type='button' onClick={e => props.setLoginShow(true)}>Login</button>) 
+        : (<div className='main-wrapper'>
+            <div className='user-name'>{props.auth.username}</div>
+            <button className='btn-logout roboto-regular' type='button' onClick={e => props.setAuth(null)}>Logout</button>
+        </div>)}        
       </header>
       <a href="#" onClick={e => setShowMobile(!showMobile)}><img src={burgerIicon} alt="burger" className='burger-icon'/></a>
       {showMobile ? <div className='main-mobile'>
         <a href='#' className='main-mobile-elem roboto-regular' onClick={e => setShowMobile(false)}>Home</a>
         <a href='#' className='main-mobile-elem roboto-regular' onClick={e => setShowMobile(false)}>Contact</a>
         <a href='#' className='main-mobile-elem roboto-regular' onClick={e => setShowMobile(false)}>About</a>
-        {!props.auth ? (<button className='btn-mobile-login roboto-regular' type='button' onClick={e => {props.setLoginShow(true); setShowMobile(false)}}>Login</button>) : (<div className='user-name-mobile'>{props.auth.username}</div>)}
+        {!props.auth 
+        ? (<button className='btn-mobile-login roboto-regular' type='button' onClick={e => {props.setLoginShow(true); setShowMobile(false)}}>Login</button>) 
+        : (<div>
+            <div className='user-name-mobile'>{props.auth.username}</div>
+            <button className='btn-mobile-logout roboto-regular' type='button' onClick={e => props.setAuth(null)}>Logout</button>
+        </div>)}
       </div> : null}
     </>
   );
