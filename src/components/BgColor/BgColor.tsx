de73d6bc53927760303906363459485613dd5e46
vika pika <dbvicdb@gmail.com>
2024-08-04 11:52:47 +0200

    make grid bg

--- src/components/BgColor/BgColor.tsx
+++ src/components/BgColor/BgColor.tsx
@@ -4,15 +4,11 @@ import './BgColor.css'
 function BgColor() {
   return (
     <div className='body-bg'>
-        <div className='color-1'></div>
-        <div className='color-2'></div>
-        <div className='color-3'></div>
-        <div className='color-4'></div>
-        <div className='color-5'></div>
-        <div className='color-6'></div>
-        <div className='color-7'></div>
-        <div className='color-8'></div>
-        <div className='color-9'></div>        
+        <div className='item-1'></div>
+        <div className='item-2'></div>
+        <div className='item-3'></div>
+        <div className='item-4'></div>
+        <div className='item-5'></div>      
       </div>
   );
 }
