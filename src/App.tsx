68ff862f8e07b29c5524a1431f257223af6341ea
vika pika <dbvicdb@gmail.com>
2024-08-06 19:50:30 +0200

    add token rotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
@@ -6,18 +6,58 @@ import BgColor from './components/BgColor/BgColor';
 import LoginForm from './components/LoginForm/LoginForm';
 
 function App() {
+  
   const getAuth = () => {
     const str = sessionStorage.getItem('auth')
     return str != null ? JSON.parse(str) : null;
   }
   
+
   const [auth, setAuth] = useState<any>(getAuth)
   const [loginShow, setLoginShow] = useState<any>(true)
+
 
   const setAndSaveAuth = (auth: any) => {
     sessionStorage.setItem('auth', JSON.stringify(auth))
     setAuth(auth)
   }
+
+
+  useEffect(() => {
+    if(auth == null) return;
+
+
+    fetch('https://dummyjson.com/auth/me', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${auth.token}`, 
+      }, 
+    })
+    .then(res => {
+      if(!res.ok) {
+        fetch('https://dummyjson.com/auth/refresh', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            refreshToken: auth.refreshToken,
+            expiresInMins: 30,
+          })
+        })
+        .then(async res => {
+          if(!res.ok) {
+            setAndSaveAuth(null) 
+          } else {
+            const data = await res.json()
+
+            setAndSaveAuth({...auth, ...data});
+          }
+        })
+      }
+    })
+  }, [])
+
 
   return (
     <div className="App">
